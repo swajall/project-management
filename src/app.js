33fb02a24783e21fe91f75adc6314dfdaa2d9491
Swajal Gupta <swajal.gupta.ug24@nsut.ac.in>
2026-07-03 19:56:18 +0530

    db connected

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import express from "express"
+import cors from "cors"
+
+const app = express();
+
+app.use(express.json({limit:"16kb"}));
+app.use(express.urlencoded({extended:true,limit: "16kb"}));
+app.use(express.static("public"));
+
+
+//cors configuration
+app.use(cors({
+    origin:process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+    credentials:true,
+    methods:["GET","POST","PUT","PATCH","DELETE","OPTIONS"],
+    allowedHeaders:["content-Type","Authorization"]
+}));
+
+app.get("/",(req,res) => {
+    res.send("welcome to express");
+})
+export default app;
