ccf3d1724e871fe43b6e1356c85a5fb157ad2c8b
Swajal Gupta <guptaswajal99@gmail.com>
2026-07-14 11:12:25 +0530

    middleware

---
+++ src/validators/index.js
@@ -0,0 +1,34 @@
+import { body } from "express-validator";
+
+const userRegisterValidator = () => {
+    return [
+        body("email")
+            .trim()
+            .notEmpty()
+            .withMessage("email is required")
+            .isEmail()
+            .withMessage("Email is invalid"),
+        body("username")
+            .trim()
+            .notEmpty()
+            .withMessage("Username is Required")
+            .isLowercase()
+            .withMessage("Username must be in lowercase")
+            .isLength({min:3})
+            .withMessage("Username must be atleast 3 characters"),
+        body("password")
+            .trim()
+            .notEmpty()
+            .withMessage("Password is required"),
+        body("fullname")
+            .optional()
+            .trim()
+            
+
+
+    ]
+}
+
+export{
+    userRegisterValidator
+}
