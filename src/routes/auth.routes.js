1a3aa3ef7b6b2505d5c142254f3e29b51b4b0c73
Swajal Gupta <swajal.gupta.ug24@nsut.ac.in>
2026-07-15 11:11:34 +0530

    all things done to handle a user: dsdd

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,14 +1,16 @@
 import { Router } from "express";
-import { registerUser,login } from "../controllers/auth.controller.js"
+import { registerUser,login,logoutUser } from "../controllers/auth.controller.js"
 import { validate } from "../middlewares/validator.middleware.js";
 import { userRegisterValidator,userLoginValidator } from "../validators/index.js";
-
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.route("/register").post(userRegisterValidator(),validate,registerUser);
 
 router.route("/login").post(userLoginValidator(), validate, login);
+
+router.route("/logout").post(verifyJWT,logoutUser);
 
 export default router;
 //testing
